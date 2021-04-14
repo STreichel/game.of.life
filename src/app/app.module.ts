19d7ad7456e443065c231efdbc78c9f84e78adf4
STreichel <sheila.welch82@outlook.com>
2021-04-14 13:49:31 -0500

    Simulator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SimulatorComponent } from './simulator/simulator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/simulator/simulator.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulator',
+  templateUrl: './simulator.component.html',
+  styleUrls: ['./simulator.component.css']
+})
+export class SimulatorComponent implements OnInit {
+  
+  numRows: number = 100;
+  numCols: number = 100;
+  grid: boolean[][];
+
+  constructor() {
+    let grid = new Array<boolean[]>(this.numRows);
+    for (var i=0; i < this.grid.length; i++) {
+      this.grid[i] = new Array<boolean>(this.numCols);
+      for (var j=0; j < this.grid[i].length; j++) {
+        this.grid[i][j] = Math.random() < 0.2;
+      }
+    }
+  }  
+  
+  ngOnInit(): void {
+  }
+
+}
