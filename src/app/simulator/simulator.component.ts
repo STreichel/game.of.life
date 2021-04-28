2b65e5b7421593acfe64278d264d3a378a218d0e
STreichel <sheila.welch82@outlook.com>
2021-04-28 13:26:13 -0500

    Rules

--- src/app/simulator/simulator.component.ts
+++ src/app/simulator/simulator.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulatorComponent implements OnInit {
   
-  numRows: number = 100;
-  numCols: number = 100;
+  numRows: number = 70;
+  numCols: number = 80;
+  
   grid: boolean[][];
+  
+  hi: number = -1;
+  hj: number = -1;
+  onHover(i: number, j: number) {
+    this.hi = i;
+    this.hj = j;
+  }
 
   constructor() {
     this.grid = new Array<boolean[]>(this.numRows);
@@ -23,20 +31,60 @@ export class SimulatorComponent implements OnInit {
   
   runOneStep(){
     let newGrid = new Array<boolean[]>(this.numRows);
-    for (var i=0; i < this.grid.length; i++) {
+    for (let i = 0; i < this.grid.length; i++) {
       newGrid[i] = new Array<boolean>(this.numCols);
-      for (var j=0; j < this.grid[i].length; j++) {
-        newGrid[i][j] = !this.grid[i][j];
-      }
+      for (let j = 0; j < this.grid[i].length; j++) {
+        let n = this.numNeighbors(i, j);
+        newGrid[i][j] = false;
+        if ( n == 3 ) {
+          newGrid[i][j] = true;  
+        }
+        if ( this.grid[i][j] && (n == 2 || n == 3 )) {
+          newGrid[i][j] = true;
+        }
+      } 
     }
+    this.grid = newGrid
+  }
+  Start(){
+    let newGrid = new Array<boolean[]>(this.numRows);
+     for (var i=0; i < this.grid.length ; i++) {
+       newGrid[i] = new Array<boolean>(this.numCols);
+      for (var j=0; j < this.grid[i].length; j++) {
+         newGrid[i][j] = Math.random() < 0.2;
+       }
+     }
     this.grid = newGrid;
+    
+   }
+
+  isAlive(i: number, j: number): boolean {
+    if (i < 0 || i >= this.numRows || j < 0 || j >= this.numCols) {
+      return false;
+    }
+    return this.grid[i][j];
   }
 
+  numNeighbors(i: number , j: number): number{
+    let n = 0;
+    for (let di = -1; di < 2; ++di){
+      for (let dj = -1; dj < 2; ++dj){
+        if ((di != 0 || dj != 0) && this.isAlive(i + di, j + dj)) {
+                ++n;
+        }
+      }
+    }
+          return n;
+  }
+
+  clear(){
+
+  }
+  
   ngOnInit(): void {
   }
 
   toggleCell(i: number, j: number): void {
     this.grid[i][j] = !this.grid[i][j]
   }
-
 }
