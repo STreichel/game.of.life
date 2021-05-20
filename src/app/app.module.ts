95bdd7c444f1eba282535356cd3dee5097b36741
STreichel <sheila.welch82@outlook.com>
2021-05-20 15:40:40 -0500

    MenuButtons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -28,4 +28,6 @@ import {MatMenuModule} from '@angular/material/menu';
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
+
