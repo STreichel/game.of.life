44515c3172cabf6f1a09fb5a6d1f34d873f0aef4
STreichel <sheila.welch82@outlook.com>
2021-06-15 12:10:57 -0500

    New Game

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -7,25 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TicTacToeComponent implements OnInit {
+  
+  board: Array<Array<string>>;
 
-  board: Array<Array<string>>
+  player1='X';
+  player2='O';
 
+  createBoard = (): Array<Array<string>> => [['', '', ''], ['', '', ''], ['', '', '']]
+  
   constructor() { 
-    this.board = this.createBoard()
-
+    this.board = this.createBoard();
+    for (let i = 0; i < this.board.length; ++i) {
+      for (let j = 0; j < this.board[i].length; ++j) {
+        let r = Math.floor(Math.random() * 3);
+        if (r == 1) {
+          this.board[i][j] = 'X';
+        } else if (r == 2) {
+          this.board[i][j] = 'O';
+        }
+      }
+    }
   }
 
   Play(){
-
-  }
+    
+  }  
 
   NewGame(){
+    this.board = this.createBoard();
+    for (let i = 0; i < this.board.length; ++i) {
+      for (let j = 0; j < this.board[i].length; ++j) {
+        this.board[i][j] = '';
+      }
+    }
+  }
 
+  takeTurn(i, j){
+    this.board = this.createBoard();
+    for (let i = 0; i < this.board.length; ++i) {
+      for (let j = 0; j < this.board[i].length; ++j) {
+        let r = Math.floor(Math.random() * 3);
+        if (r == 1) {
+          this.board[i][j] = this.player1;
+        } else if (r == 2) {
+          this.board[i][j] = this.player2;
+        }
+      }
+    }
   }
 
   ngOnInit(): void {
   }
-  
-createBoard = (): Array<Array<string>> => [['', '', ''], ['', '', ''], ['', '', '']]
-
 }
+
