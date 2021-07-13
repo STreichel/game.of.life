e3d86eb18b5e6e0d6f4c70be240f910edf18f20e
STreichel <sheila.welch82@outlook.com>
2021-07-13 11:56:40 -0500

    Pieces-starting place

--- src/app/checkers/checkers.component.ts
+++ src/app/checkers/checkers.component.ts
@@ -1,4 +1,6 @@
+import { BaseOverlayDispatcher } from '@angular/cdk/overlay/dispatchers/base-overlay-dispatcher';
 import { Component, OnInit } from '@angular/core';
+import { AttachSession } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-checkers',
@@ -7,20 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckersComponent implements OnInit {
 
+  board: Array<Array<string>>;
+
   numRows: number = 8;
   numCols: number = 8;
-
-  board: boolean[][];
+  
+  EMPTY_CELL='';
+  RED_PAWN='r';
+  RED_KING='R';
+  BLACK_PAWN='b';
+  BLACK_KING='B';
 
   constructor() {
-    this.board = new Array<boolean[]>(this.numRows);
-    for (var i=0; i < this.board.length; i++) {
-      this.board[i] = new Array<boolean>(this.numCols);
-      for (var j=0; j < this.board[i].length; j++) {
-        this.board[i][j] = this.board[i][j];
-      }
-    }
+    this.newGame();
   }
+
+  createBoard(): Array<Array<string>> {
+    let board = new Array<Array<string>>(this.numRows);
+    for (let i = 0; i < board.length; i++) {
+      board[i] = new Array<string>(this.numCols);
+      for (let j = 0; j < board[i].length; j++){
+        board[i][j] = this.EMPTY_CELL;
+      }
+    }    
+    return board;
+  };
+
+  newGame(){
+    this.board = this.createBoard();
+// Black pieces starting place
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 == 1) {
+        this.board[i][j] = this.BLACK_PAWN; 
+        }  
+      }  
+    }   
+// Red pieces starting place
+    for (let i = 5; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 == 1) {
+        this.board[i][j] = this.RED_PAWN; 
+        }
+      }  
+    }
+    return this.board;   
+  }
+
+
+
+
+
+  //inBounds() {
+//  if(i >= 0 && i < i &&
+//    j >= 0 && j < j)
+    
+//    return true;
+  
+  
+//  return false;
+  
+//}
+  
 
   ngOnInit(): void {
   }
