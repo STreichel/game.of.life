a7623af42a8a5dd59a6992dde91ddf771f2d5d09
STreichel <sheila.welch82@outlook.com>
2021-07-19 12:49:16 -0500

    commit before 1st merge

--- src/app/checkers/checkers.component.ts
+++ src/app/checkers/checkers.component.ts
@@ -1,5 +1,6 @@
 import { BaseOverlayDispatcher } from '@angular/cdk/overlay/dispatchers/base-overlay-dispatcher';
 import { Component, OnInit } from '@angular/core';
+import { ɵangular_packages_platform_browser_dynamic_platform_browser_dynamic_a } from '@angular/platform-browser-dynamic';
 import { AttachSession } from 'protractor/built/driverProviders';
 
 @Component({
@@ -24,6 +25,13 @@ export class CheckersComponent implements OnInit {
   PLAYER_NONE=0;
   PLAYER_RED=1;
   PLAYER_BLACK=2;
+
+  activePlayer = this.PLAYER_RED;
+
+  WINNER = null;
+
+  selected_i: number;
+  selected_j: number;
 
   constructor() {
     this.newGame();
@@ -61,25 +69,57 @@ export class CheckersComponent implements OnInit {
     return this.board;   
   }
 
-  whichPlayer() {
-
+  playerPiece(piece: string): number  {
+    if(piece == this.RED_PAWN || piece == this.RED_KING){
+      return this.PLAYER_RED;
+    }  
+    else if (piece == this.BLACK_PAWN || piece == this.BLACK_KING){
+      return this.PLAYER_BLACK; 
+    }
+    return this.PLAYER_NONE;
   }
 
-
-
-
-  //inBounds() {
-//  if(i >= 0 && i < i &&
-//    j >= 0 && j < j)
-    
-//    return true;
+  // Add css rule
+  canPieceMove(from_i: number, from_j: number, to_i: number, to_j: number): boolean {
+    return true;
+  }
   
-  
-//  return false;
-  
-//}
-  
+  nextPlayer(){
+    if (this.activePlayer == this.PLAYER_RED){
+      this.activePlayer = this.PLAYER_BLACK;
+    } else {
+      this.activePlayer = this.PLAYER_RED;
+    }
+  }
 
+  // Add css rule
+  cellClicked(i:number, j:number){
+    if (this.activePlayer == this.playerPiece(this.board[i][j])){
+      this.canPieceMove;
+    }
+    this.nextPlayer();
+  }
+
+// inBounds() {
+//   if (this.numRows < 8 && this.numRows > -1 && 
+//     this.numCols <8 && this.numCols > -1){
+//      return this.board[i][j];
+//   } else {
+//     return false;
+//     }
+// }
+  
+// possibleMoves() {
+//   if (inBounds && playerTurn == PLAYER_RED){
+//     check top left (numRows-1, numCols-1)
+//     check top right (numRows-1, numCols+1)
+//   }
+//   if (inBounds && playerTurn == PLAYER_BLACK){
+//     check bottom left (numRows+1, numCols-1)
+//     check bottom right (numRows+1, numCols+1)
+//   }
+// }
+  
   ngOnInit(): void {
   }
 
